5e50f1ab74b862edee91682419cf27cc0a344607
Gisele C <giseledasilvac@gmail.com>
2024-04-30 19:24:50 -0300

    funcoes em go

---
+++ funcoes.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+var(
+	nome string
+	n1 int
+	n2 int32	
+)
+
+func main(){
+	mensagem := "Aula GO"
+	fmt.Println(mensagem)
+
+	var b,f,s = true, 2.3, "Olá"
+
+	fmt.Println(b,f,s)
+
+	var total int
+	total++
+	fmt.Println("total: ", total)
+
+	nome = "Gisele"
+	fmt.Println("Hello ", nome, "!")
+
+	var x,y = 10,20
+	fmt.Println(x,y)
+	x,y = y,x
+	fmt.Println(x,y)
+}
