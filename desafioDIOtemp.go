331dfd5e490cee07c9a93d34cbe0dec63e273608
Gisele C <giseledasilvac@gmail.com>
2024-05-03 12:12:35 -0300

    Atualizando desafio - utilizando :=

--- desafioDIOtemp.go
+++ desafioDIOtemp.go
@@ -2,12 +2,13 @@ package main
 
 import "fmt"
 
+//temperatura da água em graus Kelvin
 const ebuilicaoK = 373.0
 
 func main() {
 
-	var tempK = ebuilicaoK
-	var tempC = (tempK - 273)
+	tempK := ebuilicaoK
+	tempC := (tempK - 273)
 
 	fmt.Println("A temperatura da água em °F é: ", tempK)
 	fmt.Println("A temperatura da água em °C é: ", tempC)
